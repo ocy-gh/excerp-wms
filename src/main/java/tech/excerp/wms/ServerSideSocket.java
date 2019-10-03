a3aef7091f3cd3da1aea807439c99216338f4d7a
ocy-gh <cyo95.ong@gmail.com>
2019-10-03 15:14:16 +0800

    socket

--- src/main/java/tech/excerp/wms/ServerSideSocket.java
+++ src/main/java/tech/excerp/wms/ServerSideSocket.java
@@ -11,10 +11,8 @@ public class ServerSideSocket {
             serverSocket.setSoTimeout(10000);
             while(true) {
                 System.out.println("Waiting for client on port " + serverSocket.getLocalPort() + "...");
-
                 Socket server = serverSocket.accept();
                 System.out.println("Just connected to " + server.getRemoteSocketAddress());
-
                 PrintWriter toClient =
                         new PrintWriter(server.getOutputStream(),true);
                 BufferedReader fromClient =
--- src/main/java/tech/excerp/wms/controller/RestController.java
+++ src/main/java/tech/excerp/wms/controller/RestController.java
@@ -7,12 +7,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
-import tech.excerp.wms.ClientSocket;
 import tech.excerp.wms.ServerSideSocket;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.net.UnknownHostException;
 
 @Controller
 public class RestController {
@@ -20,8 +24,6 @@ public class RestController {
     public static void main(String[] args) {
         ServerSideSocket srv = new ServerSideSocket();
         srv.run();
-        ClientSocket client = new ClientSocket();
-        client.run();
     }
 
     @PostMapping("/callback")
@@ -32,8 +34,7 @@ public class RestController {
         System.out.println("-");
         String aa = requestBodyJO.toString();
         System.out.println(aa);
-        model.addAttribute("www",aa);
-
+        model.addAttribute("testing",aa);
         return "redirect:/print";
     }
 
@@ -41,6 +42,32 @@ public class RestController {
     public String printhtml(){
         System.out.println("**************************");
         System.out.println("working working working");
+        try {
+            int serverPort = 8082;
+            InetAddress host = InetAddress.getByName("192.168.0.157");
+            System.out.println("Connecting to server on port 123123" + serverPort);
+
+            Socket socket = new Socket(host,serverPort);
+            //Socket socket = new Socket("127.0.0.1", serverPort);
+            System.out.println("Just connected to " + socket.getRemoteSocketAddress());
+            PrintWriter toServer =
+                    new PrintWriter(socket.getOutputStream(),true);
+            BufferedReader fromServer =
+                    new BufferedReader(
+                            new InputStreamReader(socket.getInputStream()));
+            toServer.println("Hello from " + socket.getLocalSocketAddress());
+            String line = fromServer.readLine();
+            System.out.println("Client received: " + line + " from Server");
+            toServer.close();
+            fromServer.close();
+            socket.close();
+        }
+        catch(UnknownHostException ex) {
+            ex.printStackTrace();
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
         return "print_from_android";
     }
 
